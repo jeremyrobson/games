481742f0fd264599dfb51eb3461c315995097246
Jeremy Robson <jeremy.robson@gmail.com>
2017-09-02 20:03:23 -0400

    pathfinding

---
+++ jerpg5/map.js
@@ -0,0 +1,20 @@
+function createObjectMap() {
+    var objectMap = createTileMap(12, 12, null);
+
+    units.forEach(function(unit) {
+        objectMap[unit.x][unit.y] = unit;
+    });
+
+    return objectMap;
+}
+
+function createTileMap(width, height, defaultValue) {
+    var binaryMap = [];
+    for (var x=0; x<width; x++) {
+        binaryMap[x] = [];
+        for (var y=0; y<height; y++) {
+            binaryMap[x][y] = defaultValue;
+        }
+    }
+    return binaryMap;
+}
--- jerpg5/unit.js
+++ jerpg5/unit.js
@@ -7,29 +7,24 @@ class Unit {
         this.agl = Math.floor(Math.random() * 9) + 1;
         this.CT = 0;
         this.CTR = 0; // Math.ceil(100 / this.agl);
-        //this.ready = false;
+        this.moverange = Math.floor(Math.random() * 5) + 5;
         this.moved = false;
         this.acted = false;
+        this.moveNodes = null;
+        this.path = null;
     }
 
     tick() {
         this.CT += this.agl;
-        //if (this.CT >= 100) {
-        //    this.ready = true;
-        //}
     }
 
     turn() {
         if (!this.moved) {
-            var path = [
-                { x: this.x + 1, y: this.y },
-                { x: this.x + 2, y: this.y },
-                { x: this.x + 3, y: this.y },
-                { x: this.x + 4, y: this.y },
-                { x: this.x + 5, y: this.y }
-            ];
+            this.moveNodes = getMoveNodes(this, this.x, this.y, this.moverange);
+            var randomNode = this.moveNodes[Math.floor(Math.random() * this.moveNodes.length)];
+            this.path = getPath(randomNode);
             var task = new Task("move", this, 999, function(unit) { //task invoke
-                var p = path.shift();
+                var p = unit.path.getNextNode();
                 if (p) {
                     unit.move(p.x, p.y);
                     return true;
@@ -39,6 +34,8 @@ class Unit {
                 }
             }, function(unit) { //task done
                 unit.moved = true;
+                unit.moveNodes = null;
+                unit.path = null;
             });
 
             return task;
@@ -47,7 +44,7 @@ class Unit {
             var task = new Task("done", this, 999, function(unit) { //task invoke
                 //todo: which direction to face
                 return false;
-            }, function(unit) {
+            }, function(unit) { //task done
                 unit.done();
             });
 
@@ -58,7 +55,6 @@ class Unit {
     done() {
         this.CT = 0;
         this.moved = false;
-        //this.ready = false;
     }
 
     move(x, y) {
@@ -79,4 +75,91 @@ class Unit {
     toString() {
         return this.sprite + "CT: " + this.CT;
     }
+}
+
+function getMoveNodes(unit, startX, startY, range) {
+    var moveNodes = [];
+    var binaryMap = createTileMap(12, 12, 0);
+    var objectMap = createObjectMap();
+
+    var startNode = {
+        x: startX,
+        y: startY,
+        steps: 0,
+        parent: null
+    };
+
+    moveNodes.push(startNode);
+
+    binaryMap[startX][startY] = 1;
+
+    var xmove = [-1, 0, 1, 0];
+    var ymove = [0, -1, 0, 1];
+    var i = 0;
+    while (i < moveNodes.length) {
+
+        var steps = moveNodes[i].steps + 1;
+        if (steps > range) {
+            i++;
+            continue;
+        }
+
+        for (var j=0; j<4; j++) {
+            var x = moveNodes[i].x + xmove[j];
+            var y = moveNodes[i].y + ymove[j];
+
+            if (x < 0 || y < 0 || x > 11 || y > 11) {
+                continue;
+            }
+
+            if (binaryMap[x][y] > 0) {
+                continue; //can't visit visited tiles
+            }
+            else {
+                binaryMap[x][y] = 1; //visit the current tile
+            }
+            
+            if (objectMap[x][y] instanceof Unit) {
+                var u = objectMap[x][y];
+
+                //can't pass through enemy units
+                if (u.team !== unit.team) {
+                    continue;
+                }
+            }
+
+            moveNodes.push({
+                x: x,
+                y: y,
+                steps: steps,
+                parent: moveNodes[i]
+            });
+        }
+
+        i++;
+    }
+
+    //remove occupied tiles (team members and self)
+    moveNodes = moveNodes.filter(function(node) {
+        return !objectMap[node.x][node.y];
+    });
+
+    return moveNodes;
+}
+
+function getPath(node) {
+    var nodes = [];
+
+    while (node.parent) {
+        nodes.push(node);
+        node = node.parent;
+    }
+
+    return {
+        index: 0,
+        nodes: nodes.reverse(),
+        getNextNode: function() {
+            return this.nodes[this.index++];
+        }
+    };
 }
